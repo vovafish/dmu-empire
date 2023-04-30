1dcadd2191eed6f84aca531387b8759fc74a02b2
Vladimirs <vladimirribakov123@mail.ru>
2023-04-30 20:50:43 +0100

    adding more items onto additional items page

--- cars/front-end/src/pages/AdditionalItemPage.jsx
+++ cars/front-end/src/pages/AdditionalItemPage.jsx
@@ -22,169 +22,224 @@ const AdditionalItemPage = () => {
             <div className={style.card}>
               <div>
                 <img
-                  src="https://m.media-amazon.com/images/I/41xPDjDGLCL._AC_.jpg"
-                  alt="Car window tinting"
+                  src="https://m.media-amazon.com/images/I/51bEEV+C88L._AC_SX425_.jpg"
+                  alt="CCHZ-WIN Mirror Dash Cam 4.3 Inches Full HD 1080P"
                 />
               </div>
               <div>
-                <p>Car window tinting</p>
+                <p>CCHZ-WIN Mirror Dash Cam 4.3 Inches Full HD 1080P</p>
+                <p>Price: 42.99£</p>
+              </div>
+            </div>
+            <div className={style.card}>
+              <div>
+                <img
+                  src="https://m.media-amazon.com/images/I/61i7HKFJ9eL._AC_SX569_.jpg"
+                  alt="Hanging Dice Pair of Hanging Couple Car Pendant with Dots Car Interior Ornament Decoration for Car Decoration"
+                />
+              </div>
+              <div>
+                <p>
+                  Hanging Dice Pair of Hanging Couple Car Pendant with Dots Car
+                  Interior Ornament Decoration for Car Decoration
+                </p>
                 <p>Price: 13.99£</p>
               </div>
             </div>
             <div className={style.card}>
               <div>
                 <img
-                  src="https://m.media-amazon.com/images/I/41xPDjDGLCL._AC_.jpg"
-                  alt="Car window tinting"
+                  src="https://m.media-amazon.com/images/I/819NdT0sBRL._AC_SX569_.jpg"
+                  alt="Apalus Air Purifying Bag, Bamboo Activated Charcoal Air Freshener, Car Air Dehumidifier, Deodorizer and Purifier Bags-100% Natural & Chemical Free Moisture, Odor Absorber, 200G"
                 />
               </div>
               <div>
-                <p>Car window tinting</p>
+                <p>
+                  Apalus Air Purifying Bag, Bamboo Activated Charcoal Air
+                  Freshener, Car Air Dehumidifier, Deodorizer and Purifier
+                  Bags-100% Natural & Chemical Free Moisture, Odor Absorber,
+                  200G
+                </p>
+                <p>Price: 10.99£</p>
+              </div>
+            </div>
+            <div className={style.card}>
+              <div>
+                <img
+                  src="https://m.media-amazon.com/images/I/51aYAOepMqL._AC_SX679_.jpg"
+                  alt="BEEWAY Blind Spot Mirrors, Round Frameless 360° Rotate Sway Adjustable HD Glass Convex Mirror Maximize RearView Universal for Car SUV Trucks Traffic Safety - Pack 2"
+                />
+              </div>
+              <div>
+                <p>
+                  BEEWAY Blind Spot Mirrors, Round Frameless 360° Rotate Sway
+                  Adjustable HD Glass Convex Mirror Maximize RearView Universal
+                  for Car SUV Trucks Traffic Safety - Pack 2
+                </p>
+                <p>Price: 3.99£</p>
+              </div>
+            </div>
+            <div className={style.card}>
+              <div>
+                <img
+                  src="https://m.media-amazon.com/images/I/616Kmzv-sRL._AC_SX679_.jpg"
+                  alt="ORSKEY Dash Cam Front and Rear 1080P Full HD Dual Dash Camera In Car Camera Dashboard Camera Dashcam for Cars 170 Wide Angle HDR with 3.0 LCD Display Night Vision Motion Detection and G-sensor"
+                />
+              </div>
+              <div>
+                <p>
+                  ORSKEY Dash Cam Front and Rear 1080P Full HD Dual Dash Camera
+                  In Car Camera Dashboard Camera Dashcam for Cars 170 Wide Angle
+                  HDR with 3.0" LCD Display Night Vision Motion Detection and
+                  G-sensor
+                </p>
+                <p>Price: 50.99£</p>
+              </div>
+            </div>
+            <div className={style.card}>
+              <div>
+                <img
+                  src="https://m.media-amazon.com/images/I/91n3ygJKUdL._AC_SX569_.jpg"
+                  alt="ShinyCar® UK: Premium 8Pcs Car Wash Tool Cleaning Kit With Bag: Microfibre Valet Washing Mitts, Exterior Drying Towel, Waffle Cloths, Alloy Wheel Brush, Detailing Brushes, Valeting Product, Gift Set"
+                />
+              </div>
+              <div>
+                <p>
+                  ShinyCar® UK: Premium 8Pcs Car Wash Tool Cleaning Kit With
+                  Bag: Microfibre Valet Washing Mitts, Exterior Drying Towel,
+                  Waffle Cloths, Alloy Wheel Brush, Detailing Brushes, Valeting
+                  Product, Gift Set
+                </p>
+                <p>Price: 26.99£</p>
+              </div>
+            </div>
+            <div className={style.card}>
+              <div>
+                <img
+                  src="https://m.media-amazon.com/images/I/81fLqG8-pGL._AC_SX679_.jpg"
+                  alt="Kärcher K 4 Premium Power Control Car & Home high pressure washer: Intelligent app support - the right solution for heavier soiling - incl. hose reel and car & home kit"
+                />
+              </div>
+              <div>
+                <p>
+                  Kärcher K 4 Premium Power Control Car & Home high pressure
+                  washer: Intelligent app support - the right solution for
+                  heavier soiling - incl. hose reel and car & home kit
+                </p>
                 <p>Price: 13.99£</p>
               </div>
             </div>
             <div className={style.card}>
               <div>
                 <img
-                  src="https://m.media-amazon.com/images/I/41xPDjDGLCL._AC_.jpg"
-                  alt="Car window tinting"
+                  src="https://m.media-amazon.com/images/I/61zYYiZEnFL._AC_SX679_.jpg"
+                  alt="Motorola MA1 Wireless Android Car Adapter - Instant Connection from Smartphone to Car Screen with Easy Setup - Direct Plug-in USB Adapter - Secure Gel Pad Included"
                 />
               </div>
               <div>
-                <p>Car window tinting</p>
-                <p>Price: 13.99£</p>
+                <p>
+                  Motorola MA1 Wireless Android Car Adapter - Instant Connection
+                  from Smartphone to Car Screen with Easy Setup - Direct Plug-in
+                  USB Adapter - Secure Gel Pad Included
+                </p>
+                <p>Price: 89.99£</p>
               </div>
             </div>
             <div className={style.card}>
               <div>
                 <img
-                  src="https://m.media-amazon.com/images/I/41xPDjDGLCL._AC_.jpg"
-                  alt="Car window tinting"
+                  src="https://m.media-amazon.com/images/I/91j6Id942zL._AC_SX679_.jpg"
+                  alt="Car Rear Children's Window Shades for Baby (Pack of Two) - Stretchable & Breathable Window Screens for Sun Protection Sunshade, Car Window Shade All-round Protection for Kids - Fits Most Vehicles"
                 />
               </div>
               <div>
-                <p>Car window tinting</p>
-                <p>Price: 13.99£</p>
+                <p>
+                  Car Rear Children's Window Shades for Baby (Pack of Two) -
+                  Stretchable & Breathable Window Screens for Sun Protection
+                  Sunshade, Car Window Shade All-round Protection for Kids -
+                  Fits Most Vehicles
+                </p>
+                <p>Price: 19.99£</p>
               </div>
             </div>
             <div className={style.card}>
               <div>
                 <img
-                  src="https://m.media-amazon.com/images/I/41xPDjDGLCL._AC_.jpg"
-                  alt="Car window tinting"
+                  src="https://m.media-amazon.com/images/I/71MvSbY3LDL._AC_SX679_.jpg"
+                  alt="Jiancrate Mobile Phone Holder for Cars, Phone Mount for Dashboard Windscreen, 360° Rotation Washable Suction Cup Base, Cars Cradle with One Button Release Compatible with iPhone 14 13 12 11 Samsung"
                 />
               </div>
               <div>
-                <p>Car window tinting</p>
-                <p>Price: 13.99£</p>
+                <p>
+                  Jiancrate Mobile Phone Holder for Cars, Phone Mount for
+                  Dashboard Windscreen, 360° Rotation Washable Suction Cup Base,
+                  Cars Cradle with One Button Release Compatible with iPhone 14
+                  13 12 11 Samsung
+                </p>
+                <p>Price: 7.95£</p>
               </div>
             </div>
             <div className={style.card}>
               <div>
                 <img
-                  src="https://m.media-amazon.com/images/I/41xPDjDGLCL._AC_.jpg"
-                  alt="Car window tinting"
+                  src="https://m.media-amazon.com/images/I/6117S0HV2TL._AC_SX425_.jpg"
+                  alt="HRB 2Packs 6S Lipo Battery 22.2V 3300mAh 60C A+ Grade LiPo RC Battery with EC5 Plug for RC Car Boat Helicopter drone Truck Airplane"
                 />
               </div>
               <div>
-                <p>Car window tinting</p>
-                <p>Price: 13.99£</p>
+                <p>
+                  HRB 2Packs 6S Lipo Battery 22.2V 3300mAh 60C A+ Grade LiPo RC
+                  Battery with EC5 Plug for RC Car Boat Helicopter drone Truck
+                  Airplane
+                </p>
+                <p>Price: 89.99£</p>
               </div>
             </div>
             <div className={style.card}>
               <div>
                 <img
-                  src="https://m.media-amazon.com/images/I/41xPDjDGLCL._AC_.jpg"
-                  alt="Car window tinting"
+                  src="https://m.media-amazon.com/images/I/612JAVJbmhL._AC_SX679_.jpg"
+                  alt="GOOLOO GT4000S Jump Starter 4000A 100W 2-Way Fast Charging, 12V Portable Car Battery Booster Pack,IP65 Power Bank Charger Box with Jumper Cables for all gasoline or 10L diesel engines"
                 />
               </div>
               <div>
-                <p>Car window tinting</p>
-                <p>Price: 13.99£</p>
+                <p>
+                  GOOLOO GT4000S Jump Starter 4000A 100W 2-Way Fast Charging,
+                  12V Portable Car Battery Booster Pack,IP65 Power Bank Charger
+                  Box with Jumper Cables for all gasoline or 10L diesel engines
+                </p>
+                <p>Price: 199.99£</p>
               </div>
             </div>
             <div className={style.card}>
               <div>
                 <img
-                  src="https://m.media-amazon.com/images/I/41xPDjDGLCL._AC_.jpg"
-                  alt="Car window tinting"
+                  src="https://m.media-amazon.com/images/I/71yWzqakQUL._AC_SX679_.jpg"
+                  alt="Dog Car Seat Cover for Dogs with Mesh Visual Window, 5 Layers Quilted Waterproof & Scratch Proof Dog Hammock with Storage Pocket and Anti-fall Zipper, Machine Washable Universal fits Cars, Trucks, SUV"
                 />
               </div>
               <div>
-                <p>Car window tinting</p>
-                <p>Price: 13.99£</p>
+                <p>
+                  Dog Car Seat Cover for Dogs with Mesh Visual Window, 5 Layers
+                  Quilted Waterproof & Scratch Proof Dog Hammock with Storage
+                  Pocket and Anti-fall Zipper, Machine Washable Universal fits
+                  Cars, Trucks, SUV
+                </p>
+                <p>Price: 23.99£</p>
               </div>
             </div>
             <div className={style.card}>
               <div>
                 <img
-                  src="https://m.media-amazon.com/images/I/41xPDjDGLCL._AC_.jpg"
-                  alt="Car window tinting"
+                  src="https://m.media-amazon.com/images/I/61+f43C+Z3S._AC_SX679_.jpg"
+                  alt="Graco Turn2Me 360° Rotating IsoFix Car Seat, Black, 1 Count "
                 />
               </div>
               <div>
-                <p>Car window tinting</p>
-                <p>Price: 13.99£</p>
-              </div>
-            </div>
-            <div className={style.card}>
-              <div>
-                <img
-                  src="https://m.media-amazon.com/images/I/41xPDjDGLCL._AC_.jpg"
-                  alt="Car window tinting"
-                />
-              </div>
-              <div>
-                <p>Car window tinting</p>
-                <p>Price: 13.99£</p>
-              </div>
-            </div>
-            <div className={style.card}>
-              <div>
-                <img
-                  src="https://m.media-amazon.com/images/I/41xPDjDGLCL._AC_.jpg"
-                  alt="Car window tinting"
-                />
-              </div>
-              <div>
-                <p>Car window tinting</p>
-                <p>Price: 13.99£</p>
-              </div>
-            </div>
-            <div className={style.card}>
-              <div>
-                <img
-                  src="https://m.media-amazon.com/images/I/41xPDjDGLCL._AC_.jpg"
-                  alt="Car window tinting"
-                />
-              </div>
-              <div>
-                <p>Car window tinting</p>
-                <p>Price: 13.99£</p>
-              </div>
-            </div>
-            <div className={style.card}>
-              <div>
-                <img
-                  src="https://m.media-amazon.com/images/I/41xPDjDGLCL._AC_.jpg"
-                  alt="Car window tinting"
-                />
-              </div>
-              <div>
-                <p>Car window tinting</p>
-                <p>Price: 13.99£</p>
-              </div>
-            </div>
-            <div className={style.card}>
-              <div>
-                <img
-                  src="https://m.media-amazon.com/images/I/41xPDjDGLCL._AC_.jpg"
-                  alt="Car window tinting"
-                />
-              </div>
-              <div>
-                <p>Car window tinting</p>
-                <p>Price: 13.99£</p>
+                <p>
+                  Graco Turn2Me 360° Rotating IsoFix Car Seat, Black, 1 Count
+                </p>
+                <p>Price: 149.99£</p>
               </div>
             </div>
           </div>
