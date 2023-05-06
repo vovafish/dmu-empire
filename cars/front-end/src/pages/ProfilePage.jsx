32b494e6b86b1b019916bf00709d13b7758786d5
Vladimirs <vladimirribakov123@mail.ru>
2023-05-06 18:13:01 +0100

    changed title and added icon for website

--- cars/front-end/src/pages/ProfilePage.jsx
+++ cars/front-end/src/pages/ProfilePage.jsx
@@ -6,14 +6,19 @@ const ProfilePage = () => {
   const user = useUser();
   return (
     <div className="mainContainer">
-      <p>First name: {user.first_name}</p>
-      <p>Last name: {user.last_name}</p>
-      <p>Status: {user.isVerified ? 'Verified' : 'Not Verified'}</p>
-      <p>Type: {user.isAdmin ? 'Administrator' : 'Customer'}</p>
-      <p>
-        Phone number: {user.phone_number ? user.phone_number : 'Not provided'}
-      </p>
-      {console.log(user)}
+      <main className={style.main}>
+        <div>
+          <p>First name: {user.first_name}</p>
+          <p>Last name: {user.last_name}</p>
+          <p>Status: {user.isVerified ? 'Verified' : 'Not Verified'}</p>
+          <p>Type: {user.isAdmin ? 'Administrator' : 'Customer'}</p>
+          <p>
+            Phone number:{' '}
+            {user.phone_number ? user.phone_number : 'Not provided'}
+          </p>
+          {console.log(user)}
+        </div>
+      </main>
     </div>
   );
 };
